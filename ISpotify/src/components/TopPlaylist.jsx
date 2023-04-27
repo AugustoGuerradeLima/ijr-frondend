9a462c370802a4e23ca948bad71bc3883c9b4741
João Vítor Bicalho <joaovitorbicalho.dasilva@gmail.com>
2023-04-27 16:32:02 -0300

    Reorganizando o projeto, add pasta de páginas

--- ISpotify/src/components/TopPlaylist.jsx
+++ ISpotify/src/components/TopPlaylist.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import AlbumCover from "../images/albumcover.png";
 
-import './Playlist.css'
+import './TopPlaylist.css'
 
-const Playlist = (props) => {
+const TopPlaylist = (props) => {
   return (
-    <div className="playlist-container">
+    <div className="top-playlist-container">
       <div className="album-cover">
         <p className="album">
           <img src={AlbumCover} alt="album cover" />
@@ -15,13 +15,16 @@ const Playlist = (props) => {
         <p className="playlist">Playlist</p>
         <h1 className="daily-mix">{props.name}</h1>
         <div className="more-info">
-          <p>{props.nrtists}</p>
-          <p>Spotify - {props.numberOfSongs} músicas</p>
-          <p className="duration">{props.duration}</p>
+          <p>{props.artists}</p>
+          <div className="number-songs-hours">
+            <p>Spotify - {props.numberOfSongs} músicas</p>
+            <p className="duration">{props.duration}</p>
+          </div>
+
         </div>
       </div>
     </div>
   );
 };
 
-export default Playlist
+export default TopPlaylist
---
+++ ISpotify/src/pages/Playlist/Playlist.jsx
@@ -0,0 +1,107 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+import DownloadIcon from "../../images/download.svg";
+import HeartIconUnliked from "../../images/heart.svg";
+import HeartIconLiked from "../../images/heart-v2.svg";
+import PlayButton from "../../images/playButton.png";
+import ThreeDots from "../../images/threeDots.png";
+import ClockIcon from "../../images/clock.svg";
+
+import "./Playlist.css"
+
+import TopPlaylist from '../../components/TopPlaylist';
+import SongPlaylist from '../../components/SongPlaylist';
+
+const Playlist = () => {
+
+  const [songs, setSongs] = useState([
+    {
+      id: 1,
+      songName: "The Zephyr Song",
+      artistName: "Red Hot Chili Peppers",
+      albumName: "By the way",
+    },
+    {
+      id: 2,
+      songName: "Talk",
+      artistName: "Coldplay",
+      albumName: "X&Y",
+    },
+    {
+      id: 3,
+      songName: "Cidade Negra",
+      artistName: "Firmamento",
+      albumName: "Cidade Negra Acústico MTV",
+    }
+  ])
+
+  const removeSong = (id) => {
+    const updatedSongs = songs.filter((song) => song.id !== id)
+    setSongs(updatedSongs)
+  }
+
+  const [likeHeart, setLikeHeart] = useState(false)
+
+  const handleLikeHeart = () => {
+    if (likeHeart == false) {
+      setLikeHeart(true)
+    } else {
+      setLikeHeart(false)
+    }
+
+  }
+
+  return (
+    <div className='playlist-container'>
+      <TopPlaylist
+        name="Daily Mix 1"
+        artists="Red Hot Chili Peppers, Coldplay e mais"
+        numberOfSongs="50"
+        duration="1h 7min"
+      />
+
+      <div className="buttons-line">
+        <p className="play-button">
+          <img className='PB' src={PlayButton} alt="play button" />
+        </p>
+        {!likeHeart ?
+          (<p className="heart-button">
+            <img className='HB' src={HeartIconUnliked} alt="unlike button" id={likeHeart} onClick={handleLikeHeart} />
+          </p>) :
+          (<p className="heart-button">
+            <img className='HB' src={HeartIconLiked} alt="like button" id={likeHeart} onClick={handleLikeHeart} />
+          </p>)}
+        <p className="download-button">
+          <img className='DB' src={DownloadIcon} alt="download button" />
+        </p>
+        <p className="three-dots">
+          <img className='TD' src={ThreeDots} alt="know more button" />
+        </p>
+      </div>
+
+      <div className="songs-top-line">
+        <p className='title'>#TÍTULO</p>
+        <p className='album'>ÁLBUM</p>
+        <p className="clock-icon">
+          <img src={ClockIcon} alt="clock icon" />
+        </p>
+      </div>
+
+      <hr />
+
+      {songs.map(((song, index) => (
+        <SongPlaylist
+          key={song.id}
+          number={index + 1}
+          songName={song.songName}
+          artistName={song.artistName}
+          albumName={song.albumName}
+          removeSong={() => removeSong(song.id)}
+        />
+      )))}
+    </div>
+  )
+}
+
+export default Playlist
