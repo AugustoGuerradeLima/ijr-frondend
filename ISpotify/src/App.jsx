7c0fd51dddb62f7d29712fd0d386e23cae0643e5
João Vítor Bicalho <joaovitorbicalho.dasilva@gmail.com>
2023-04-28 19:54:12 -0300

    reestruturando css pagina playlist

--- ISpotify/src/App.jsx
+++ ISpotify/src/App.jsx
@@ -2,12 +2,14 @@ import { useState, useEffect } from 'react'
 
 import "./App.css";
 import Playlist from './pages/Playlist/Playlist';
+import FavMusics from './pages/FavMusics/FavMusics';
 
 function App() {
 
   return (
     <div className="App">
       <Playlist />
+      {/* <FavMusics /> */}
     </div>
   );
 }
